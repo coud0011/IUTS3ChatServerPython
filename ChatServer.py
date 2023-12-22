807b2b2f138109c05170d6b3ec7f9e569ab122fb
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-12-22 10:02:22 +0100

    config du serv en cours

--- ChatServer.py
+++ ChatServer.py
@@ -1,6 +1,11 @@
 import asyncio
 
 
+def clear(alias):
+    alias, r, t = alias.partition("\n")
+    return alias
+
+
 class Server:
     def __init__(self, ip, port):
         self.ip = ip
@@ -16,32 +21,44 @@ class Server:
             self.reader = reader
             self.alias = alias
 
+        async def write(self, message):
+            self.writer.write(message.encode())
+            await self.writer.drain()
+
     def valid(self, alias):
         return not alias in self.clients.items()
 
-    async def write(self, message, alias):
-        self.clients[alias].writer.write(message.encode())
-        await self.clients[alias].writer.drain()
+    async def broadcast(self, message, sender=None, returning=False):
+        if sender is None:
+            sender = ''
+        for elt in self.clients:
+            print(returning, sender.alias, elt, not (returning and sender == elt))
+            if not (returning and sender == elt):
+                await self.clients[elt].write(message)
 
     async def register_client(self, reader, writer):
         print('New connection:')
-        alias = await reader.read()
+        alias = (await reader.readline()).decode()
+        alias = clear(alias)
+        print(alias)
         if self.valid(alias):
             clt = Server.Client(alias, reader, writer)
             self.clients[alias] = clt
-            ret = ""
-            for elt, key in self.clients.items():
-                ret += elt + " "
-            await self.clients[alias].write(f"#alias {alias}")
-            await self.clients[alias].write(f"#list {ret}{alias}")
-            await self.clients[alias].write(f"#connected {alias}")
+            await clt.write(f"#alias {alias}\n")
+            await asyncio.sleep(0.01)
+            await self.clients_list(clt)
+            await asyncio.sleep(0.01)
+            await self.broadcast(f"#connected {alias}\n", clt, True)
+            print('fin connection')
         else:
-            await self.clients[alias].write("#error invalid_alias")
+            await self.clients[alias].write("#error invalid_alias\n")
 
     async def handle_client(self, reader, writer):
-        request = reader.read()
         client_alias = await self.register_client(reader, writer)
         while client_alias in self.clients:
+            print('début boucle')
+            request = (await reader.readline()).decode()
+            print(request)
             await self.process_request(request, client_alias)
 
     async def start_server(self):
@@ -52,32 +69,39 @@ class Server:
             message = request.partition(' ')
             if message[0] in self.client_commands.keys():
                 self.client_commands[message[0]](message, sender)
+            else:
+                await sender.write('#error invalid_command\n')
         else:
             await self.broadcast(message=request, sender=sender)
 
-    async def broadcast(self, message, sender=None):
-        if sender is None:
-            sender = ''
-        for elt in self.clients.keys():
-            await self.write(message, elt)
-
     async def alias(self, message, sender):
-        await self.write("#alias "+message[2], sender.alias)
-        await self.broadcast("#renamed "+sender.alias+' '+message[2], sender.alias)
+        await sender.write("#alias " + message[2] + '\n')
+        await self.broadcast("#renamed " + sender.alias + ' ' + message[2] + '\n', sender.alias)
         self.clients.pop(sender.alias)
-        self.clients[message[2]]=sender
+        self.clients[message[2]] = sender
 
-    async def clients_list(self, message, sender):
-        await self.write(message, sender.alias)
+    async def clients_list(self, sender):
+        ret = ""
+        print(self.clients)
+        for elt in self.clients:
+            print(elt)
+            ret += elt + " "
+        await sender.write('#list ' + ret + '\n')
 
     async def private_message(self, message, sender):
         reciev, sep, send = message[2].partition
         if reciev not in self.clients.keys():
-            await self.write('#error invalid_recipient', sender.alias)
-
+            await sender.write('#error invalid_recipient\n')
+        elif send == '' or send is None:
+            await sender.write('#error missing_argument\n')
+        else:
+            await sender.write(reciev)
 
     async def quit(self, message, sender):
-        pass
+        sender.writer.close()
+        sender.reader.close()
+        self.clients.pop(sender.alias)
+        await self.broadcast(message, sender)
 
 
 async def main():
@@ -85,6 +109,7 @@ async def main():
     await s.start_server()
     async with s.server:
         await s.server.serve_forever()
+        print(s.clients)
 
 
 asyncio.run(main())
